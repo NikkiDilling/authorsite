e082408a650cac7c82a08ee1bccfc0fb5bf109df
Nikki Dilling-Hansen <ndh@peoplenet.dk>
2025-10-18 19:02:22 +0200

    styling and main structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import classes from './../css/Header.module.scss';
+
+export default function Header() {
+ 
+
+  return (
+    <div className={classes.container}>
+        
+    </div>
+  );
+}
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import classes from './../css/MainPage.module.scss';
+
+export default function MainPage() {
+ 
+
+  return (
+    <div className={classes.container}>
+
+    </div>
+  );
+}
