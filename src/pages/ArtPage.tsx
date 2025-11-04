7731be68d4bd3c7463a35c0b06627bd1637401da
Nikki Dilling-Hansen <ndh@peoplenet.dk>
2025-11-04 17:49:35 +0100

    fixed art wall

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,44 +1,55 @@
-
-import ArtContainer from '../components/ArtContainer';
-import classes from './../css/MainPage.module.scss';
-
+import classes from './../css/ArtPage.module.scss';
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 export default function ArtPage() {
 
-
+    const itemData = [
+        {
+            img: 'src/assets/Tayrem(art).png',
+            title: 'Tayrem - Half body',
+            author: 'ig: Kostojj',
+        },
+        {
+            img: 'src/assets/Tayrem & Elayah.png',
+            title: 'Tayrem & Elayah',
+            author: 'Nicole D. Hansen',
+        },
+        {
+            img: 'src/assets/Vanadian sigil.png',
+            title: 'Vanadium sigil',
+            author: 'Nicole D. Hansen',
+        },
+        {
+            img: 'src/assets/first meeting.jpg',
+            title: 'First meeting',
+            author: 'Nicole D. Hansen',
+        },
+        {
+            img: 'src/assets/Tayrem (chained).jpg',
+            title: 'Tayrem ch.7 scene',
+            author: 'Nicole D. Hansen',
+        }
+    ];
     return (
         <div className={classes.container}>
-
-            <ArtContainer
-                title="Tayrem - Project Sunset"
-                image="src\assets\Tayrem(art).png"
-                date="October 2025"
-                excerpt="Made by Kostojj"
-            />
-            <ArtContainer
-                title="Tayrem & Elayah"
-                image="src\assets\Tayrem & Elayah.png"
-                date="November 2025"
-                excerpt="Made by Nicole D. Hansen"
-            />
-            <ArtContainer
-                title="Vanadium sigil"
-                image="src\assets\Vanadian sigil.png"
-                date="October 2025"
-                excerpt="Made by Nicole D. Hansen"
-            />
-            <ArtContainer
-                title="First meeting"
-                image="src\assets\first meeting.jpg"
-                date="2024"
-                excerpt="Made by Nicole D. Hansen"
-            />
-            <ArtContainer
-                title="Tayrem ch.7 scene"
-                image="src\assets\Tayrem (chained).jpg"
-                date="2024"
-                excerpt="Made by Nicole D. Hansen"
-            />
+            <Box className={classes.box}>
+                <ImageList variant="masonry" cols={3} gap={2}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${item.img}?w=248&fit=crop&auto=format`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                            <ImageListItemBar position="below" title={item.author} />
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+            </Box>
 
         </div>
     );
