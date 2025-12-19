3b7c609b9bd25fc19f2c7cf31e86d6d1a49edc57
Nikki Dilling-Hansen <ndillinghansen@gmail.com>
2025-12-19 19:44:22 +0100

    book page updates

--- src/pages/ProjectSunsetPage.tsx
+++ src/pages/ProjectSunsetPage.tsx
@@ -4,73 +4,52 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
-const steps = [
-  {
-    label: 'Intro',
-  },
-  {
-    label: 'Map'
-  },
-  {
-    label: 'Series'
-  },
-  {
-    label: 'Coming soon'
-  }
-];
-
-
-
 export default function ProjectSunsetPage() {
-
-  const jumpToContent = (id: string) => {
-    window.location.hash = "#" + id;
-  };
 
   return (
     <div className={classes.container} id='Intro'>
-
+      <div className={classes.banner}>
+        <img src="/assets/\Elven Wastelands banner 1.png" alt="Elven Wastelands banner" />
+      </div>
       <main>
         <div>
+
           <div className={classes.quotes}>
             <div>"Even demons don't like the dark."</div>
           </div>
 
-          <img src="/assets/\divider.png" className={classes.divider} id='Map' />
-          <div>Enter a world of elves, elemental spirits and intrigue!</div>
-          <br/>
-          <div>Project Sunset is the codename the first book of the Elven Wastelands series.</div>
-              <img src="/assets/\divider.png" className={classes.divider} />
+          <img src="/assets/\divider.png" className={classes.divider} />
+          <div className={classes.textContainer}>
+            <div>Enter a world of elves, elemental spirits and intrigue!</div>
+            <br />
+            <div>Project Sunset is the codename the first book of the Elven Wastelands series.</div>
+          </div>
+          <img src="/assets/\divider.png" className={classes.divider} />
           <div>
-            <img src="/assets/world map.png" alt="World Map" />
+            <img className={classes.map} src="/assets/world map.png" alt="World Map" />
             <div style={{ textAlign: "center" }} >Map</div>
           </div>
 
           <img src="/assets/\divider.png" className={classes.divider} />
 
-          <h2> Elven Wastelands series</h2>
-          <div>Currently </div>
+          <div className={classes.textContainer}>
+            <h2> Elven Wastelands series</h2>
+            <div>Currently </div>
+          </div>
 
-          <img src="/assets/\divider.png" className={classes.divider} id='Series' />
+          <img src="/assets/\divider.png" className={classes.divider} />
 
-          <div> some text </div>
-          <img src="/assets/\divider.png" className={classes.divider} id='Coming' />
+          <div className={classes.textContainer}>
+            <div> some text </div>
+          </div>
 
-          <div> some text </div>
+          <img src="/assets/\divider.png" className={classes.divider} />
+
+          <div className={classes.textContainer}>
+            <div> some text </div>
+          </div>
         </div>
-        <section>
-          <Box sx={{ maxWidth: 400 }}>
-            <Stepper /* activeStep={activeStep} */ orientation="vertical" >
-              {steps.map((step) => (
-                <Step key={step.label} onClick={() => jumpToContent(step.label)} >
-                  <StepLabel>
-                    {step.label}
-                  </StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-          </Box>
-        </section>
+
       </main>
 
 
