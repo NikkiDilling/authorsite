2254abd91cb94ad486e4afd51dd143da3ffe39fc
Nikki Dilling-Hansen <ndillinghansen@gmail.com>
2026-01-20 14:17:20 +0100

    finally improved images!

--- src/pages/ArtPage.tsx
+++ src/pages/ArtPage.tsx
@@ -1,13 +1,14 @@
 import classes from './../css/ArtPage.module.scss';
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
 import tayremArt from '../assets/Tayrem-commission.webp';
 import tayremNElayah from '../assets/Tayrem & Elayah.webp';
 import sigil from '../assets/Vanadian sigil.webp';
 import firstMeeting from '../assets/first meeting.webp';
 import tayremChained from '../assets/Tayrem-chained.webp';
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import 'swiper/swiper-bundle.css';
 
 export default function ArtPage() {
 
@@ -15,7 +16,8 @@ export default function ArtPage() {
         {
             img: tayremArt,
             title: 'Tayrem - Half body',
-            author: 'ig: Kostojj',
+            author: 'Kostojj',
+            ig: "https://www.instagram.com/kostojjn/"
         },
         {
             img: tayremNElayah,
@@ -44,21 +46,56 @@ export default function ArtPage() {
                 Here are some artworks either made by me or commissioned from other artists for my books.
             </div>
 
-            <Box className={classes.box}>
-                <ImageList variant="masonry" cols={3} gap={15}>
-                    {itemData.map((item) => (
-                        <ImageListItem key={item.img}>
+            <Swiper
+                modules={[Navigation]}
+                slidesPerView={3}
+                spaceBetween={30}
+                grabCursor
+                loop={true}
+                navigation
+
+                className={classes.swiper}
+            >
+
+                {itemData.map((item, index) => (
+                    <SwiperSlide>
+                        <div className={classes.imageWrapper}>
                             <img
-                                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                src={`${item.img}?w=248&fit=crop&auto=format`}
+                                key={index}
+                                srcSet={item.img}
+                                src={item.img}
                                 alt={item.title}
                                 loading="lazy"
                             />
-                            <ImageListItemBar position="below" title={item.author} />
-                        </ImageListItem>
-                    ))}
-                </ImageList>
-            </Box>
+
+                        </div>
+                        <div className={classes.caption}>
+                            <div className={classes.title}>"{item.title}"</div>
+                            {item.ig ?
+                                (
+                                    <div style={{display: "flex", flexDirection: "row"}}>
+                                        <div className={classes.author}>by: {item.author}</div>
+                                        <a href='https://www.instagram.com/ndhansen_' target='_blank' style={{marginLeft: "10px"}}>
+                                            <FontAwesomeIcon
+                                                icon={faInstagram}
+                                                className={classes.icon}
+                                            />
+                                        </a>
+                                    </div>
+
+                                ) :
+                                (
+                                    <div className={classes.author}>by: {item.author}</div>
+
+                                )
+                            }
+                        </div>
+                    </SwiperSlide>
+                ))}
+
+
+            </Swiper>
+
 
         </div>
     );
