acd7025769df66631d912fd43245400b79e4a2be
Nikki Dilling-Hansen <ndillinghansen@gmail.com>
2026-01-21 09:13:36 +0100

    added banner to main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,65 +3,72 @@ import classes from './../css/MainPage.module.scss';
 import NewsletterForm from '../components/NewsLetterForm';
 import divider from "../assets/divider.webp";
 import pfp from "../assets/pfp.gif";
+import banner from "../assets/Elven Wastelands banner 1.webp";
 
 export default function MainPage() {
 
   return (
-    <div className={classes.container}>
+    <div>
 
-      <div className={classes.mainSection}>
-
-        <div className={classes.section}>
-          <img src={pfp} alt="profile image" className={classes.pfp} />
-          <div className={classes.sectionContent}>
-            <div>
-              <div className={classes.text}>Hi! My name is Nicole D. Hansen, and I'm an indie author, from Denmark. </div>
-              <div className={classes.text}>I write delicious slow burn romantasy with characters and plot-lines that will heal your inner child. </div>
-              <div className={classes.text}>I also daylight as a software developer. </div>
-            </div>
-            <Link to='/About' className={classes.readMoreBtn} >Learn more about me</Link>
-          </div>
-
-        </div>
-
-        <div className={classes.mobileView}>
-          <img src={divider} className={classes.divider} />
-          <h3>Nicole's newsletter</h3>
-          <p>Sign up for my newsletter and be the first to get book updates, writing tips, exclusive content, deals and so much more!</p>
-          <NewsletterForm />
-        </div>
-
-        <img src={divider} className={classes.divider} />
-
-        <div className={classes.section}>
-          <div className={classes.sectionContent}>
-            <h3>"Project Sunset"</h3>
-            <div>
-              <div>"Project Sunset" the first book of the Elven Wastelands series. It is my debut romantasy novel I’ve been writing for over 5 years...</div>
-            </div>
-            <Link to='/Books' className={classes.readMoreBtn}>Read more</Link>
-          </div>
-        </div>
+      <div className={classes.banner}>
+        <img src={banner} alt="Elven Wastelands banner" />
       </div>
+      <div className={classes.container}>
 
+        <div className={classes.mainSection}>
 
-      <div className={classes.recentPosts}>
-
-        <div className={classes.desktopView}>
-          <h3>Sign up to my newsletter</h3>
-          <p className={classes.mobileView}>Sign up for my newsletter and be the first to get book updates, writing tips, exclusive content, deals and so much more!</p>
-
-          <NewsletterForm />
-        </div>
-
-        <div className=" unreleased">
-          <img src={divider} className={classes.divider} />
-          <h4>Recent blog posts</h4>
-          <div>
-            {/* Blog posts will go here */}
+          <div className={classes.section}>
+            <img src={pfp} alt="profile image" className={classes.pfp} />
+            <div className={classes.sectionContent}>
+              <div>
+                <div className={classes.text}>Hi! My name is Nicole D. Hansen, and I'm an indie author, from Denmark. </div>
+                <div className={classes.text}>I write delicious slow burn romantasy with characters and plot-lines that will heal your inner child. </div>
+                <div className={classes.text}>I also daylight as a software developer. </div>
+              </div>
+              <Link to='/About' className={classes.readMoreBtn} >Learn more about me</Link>
+            </div>
 
           </div>
 
+          <div className={classes.mobileView}>
+            <img src={divider} className={classes.divider} />
+            <h3>Nicole's newsletter</h3>
+            <p>Sign up for my newsletter and be the first to get book updates, writing tips, exclusive content, deals and so much more!</p>
+            <NewsletterForm />
+          </div>
+
+          <img src={divider} className={classes.divider} />
+
+          <div className={classes.section}>
+            <div className={classes.sectionContent}>
+              <h3>"Project Sunset"</h3>
+              <div>
+                <div>"Project Sunset" the first book of the Elven Wastelands series. It is my debut romantasy novel I’ve been writing for over 5 years...</div>
+              </div>
+              <Link to='/Books' className={classes.readMoreBtn}>Read more</Link>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={classes.recentPosts}>
+
+          <div className={classes.desktopView}>
+            <h3>Sign up to my newsletter</h3>
+            <p className={classes.mobileView}>Sign up for my newsletter and be the first to get book updates, writing tips, exclusive content, deals and so much more!</p>
+
+            <NewsletterForm />
+          </div>
+
+          <div className=" unreleased">
+            <img src={divider} className={classes.divider} />
+            <h4>Recent blog posts</h4>
+            <div>
+              {/* Blog posts will go here */}
+
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
